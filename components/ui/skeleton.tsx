a81a27f6a7a60b9a56f9e071d7c5e49b5374114e
René Aaron <rene@twentyuno.net>
2024-05-09 13:51:01 +0200

    fix: skeleton style

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -30,7 +30,7 @@ function Skeleton({
   return (
     <Animated.View
       style={style}
-      className={cn('rounded-md bg-secondary dark:bg-muted', className)}
+      className={cn('rounded-md bg-primary/10', className)}
       {...props}
     />
   );
