2308c1a7c7511ea2a804d62a9158b0e34d7cdd66
Roland Bewick <roland.bewick@gmail.com>
2024-09-10 13:28:34 +0700

    fix: error handling
    previously we do not properly catch errors in the swr methods.
    
    errorToast also threw an uncaught exception if it did not receive an error, which lead to the app hanging

--- lib/errorToast.ts
+++ lib/errorToast.ts
@@ -1,8 +1,10 @@
 import Toast from "react-native-toast-message";
 
-export function errorToast(error: Error) {
+export function errorToast(error: Error | unknown) {
   Toast.show({
     type: "error",
-    text1: error.message,
+    text1:
+      (error as Error | undefined)?.message ||
+      "An unknown error occured. Please check your internet connection or try restarting Alby Go",
   });
 }
