732adeea06f41cb4361b6b3c7824e71f5ff8072c
René Aaron <rene@twentyuno.net>
2024-09-17 14:04:05 +0200

    fix: copy & component usage

--- pages/settings/Security.tsx
+++ pages/settings/Security.tsx
@@ -6,29 +6,33 @@ import Screen from "~/components/Screen";
 import { useAppStore } from "~/lib/state/appStore";
 import { TriangleAlert } from "~/components/Icons";
 import { cn } from "~/lib/utils";
+import { Card, CardDescription, CardHeader, CardTitle } from "~/components/ui/card";
 
 export function Security() {
   const isEnabled = useAppStore((store) => store.isSecurityEnabled);
-  const isSupported = useAppStore((store) => store.isBiometricSupported);
+  const isSupported = false;
   return (
     <View className="flex-1 p-6">
       <Screen title="Security" />
       {!isSupported && (
-        <View className="flex-row items-center border border-muted-foreground rounded-lg p-3 mb-4 gap-3">
-          <TriangleAlert className="text-foreground" />
-          <View className="flex-col">
-            <Text className="font-bold text-base">Can't add security</Text>
-            <Text className="text-sm">
-              Add phone lock in device settings to secure access
-            </Text>
-          </View>
-        </View>
+        <Card className="mb-6">
+          <CardHeader>
+            <CardTitle className="flex flex-row gap-3 items-center">
+              <TriangleAlert size={16} className="text-foreground" />
+              <Text>{" "}Setup Device Security</Text>
+            </CardTitle>
+            <CardDescription>
+              To protect your wallet, please set up a phone lock in your device settings first.
+            </CardDescription>
+          </CardHeader>
+        </Card>
       )}
       <View className="flex-1">
-        <View className='flex-row items-center justify-between gap-2'>
+        <View className="flex-row items-center justify-between gap-2">
           <Label
-            className={cn('text-2xl', !isSupported && 'opacity-50')}
-            nativeID='security'
+            className={cn("text-2xl", !isSupported && "text-muted-foreground")}
+            nativeID="security"
+            disabled={isSupported}
           >
             <Text className="text-lg">Require phone lock to access</Text>
           </Label>
@@ -38,7 +42,7 @@ export function Security() {
             onCheckedChange={() => {
               useAppStore.getState().setSecurityEnabled(!isEnabled);
             }}
-            nativeID='security' />
+            nativeID="security" />
         </View>
       </View>
     </View>
