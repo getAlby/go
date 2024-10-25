d7c41ccef5d289c9623894598bf50024015d9650
René Aaron <100827540+reneaaron@users.noreply.github.com>
2024-10-25 10:52:27 +0200

    fix: success layout (#160)
    * fix: success layout
    
    * fix: center content on screen

--- animations/Tick.tsx
+++ animations/Tick.tsx
@@ -6,8 +6,9 @@ export function Tick() {
       autoPlay
       loop={false}
       style={{
-        width: 400,
-        height: 400,
+        width: "100%",
+        height: "100%",
+        minHeight: 250,
       }}
       source={require("../assets/animations/success.json")}
     />
--- pages/receive/ReceiveSuccess.tsx
+++ pages/receive/ReceiveSuccess.tsx
@@ -1,6 +1,6 @@
 import { Invoice } from "@getalby/lightning-tools";
 import { router, useLocalSearchParams } from "expo-router";
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import { Tick } from "~/animations/Tick";
 import Screen from "~/components/Screen";
 import { Button } from "~/components/ui/button";
@@ -17,32 +17,37 @@ export function ReceiveSuccess() {
   return (
     <View className="flex-1 flex flex-col">
       <Screen
-        title="Payment Successful"
+        title="Receive successful"
       />
-      <View className="flex-1 justify-center items-center gap-2">
-        <View className="-mt-32">
+      <ScrollView contentContainerClassName="flex justify-center items-center gap-8 p-6">
+        <View className="flex-1 w-full">
           <Tick />
         </View>
-        <Text className="text-3xl font-bold2 text-muted-foreground -mt-32">
+        <Text className="text-3xl font-bold2 text-muted-foreground">
           Received
         </Text>
-        <View className="flex flex-row gap-2 mt-10">
-          <Text className="text-receive text-3xl font-semibold2">
-            + {decodedInvoice.satoshi}
-          </Text>
-          <Text className="text-3xl font-semibold2 text-muted-foreground">
-            sats
-          </Text>
+        <View className="flex flex-col items-center gap-2 mt-10">
+          <View className="flex flex-row items-end justify-center">
+            <Text className="text-3xl text-receive font-semibold2">
+              + {new Intl.NumberFormat().format(+decodedInvoice.satoshi)}{" "}
+            </Text>
+            <Text className="text-2xl text-muted-foreground font-semibold2">sats</Text>
+          </View>
+          {getFiatAmount &&
+            <Text className="text-2xl text-muted-foreground font-semibold2">{getFiatAmount(+decodedInvoice.satoshi)}</Text>
+          }
         </View>
-        {getFiatAmount &&
-          <Text className="text-muted-foreground text-2xl font-semibold2">
-            {getFiatAmount(decodedInvoice.satoshi) ?? ""}
-          </Text>
-        }
         {decodedInvoice.description && (
-          <Text className="mt-4">{decodedInvoice.description}</Text>
+          <View className="flex flex-col gap-2 items-center">
+            <Text className="text-muted-foreground text-center font-semibold2">
+              Description
+            </Text>
+            <Text className="text-foreground text-center text-2xl font-medium2">
+              {decodedInvoice.description}
+            </Text>
+          </View>
         )}
-      </View>
+      </ScrollView>
       <View className="flex flex-col gap-3 m-6">
         <Button
           size="lg"
