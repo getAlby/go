a143df4593e3ea2659effe6547a8fb3c2a22cbc5
Roland Bewick <roland.bewick@gmail.com>
2025-03-11 21:32:45 +0700

    fix: do not show error toasts when in-progress nwc request was backgrounded

--- hooks/useTransactions.ts
+++ hooks/useTransactions.ts
@@ -1,31 +1,18 @@
 import { useAppStore } from "lib/state/appStore";
 import useSWR from "swr";
 import { TRANSACTIONS_PAGE_SIZE } from "~/lib/constants";
-import { errorToast } from "~/lib/errorToast";
+import { createNwcFetcher } from "~/lib/createNwcFetcher";
 
-type FetchArgs = Parameters<typeof fetch>;
-
-const fetcher = async (...args: FetchArgs) => {
-  const nwcClient = useAppStore.getState().nwcClient;
-  if (!nwcClient) {
-    throw new Error("No NWC client");
-  }
-
+const fetcher = createNwcFetcher(async (nwcClient, args) => {
   const transactionsUrl = new URL("http://" + (args[0] as string));
   const page = +(transactionsUrl.searchParams.get("page") as string);
-
-  try {
-    const transactions = await nwcClient.listTransactions({
-      limit: TRANSACTIONS_PAGE_SIZE,
-      offset: (page - 1) * TRANSACTIONS_PAGE_SIZE,
-      unpaid_outgoing: true,
-    });
-    return transactions;
-  } catch (error) {
-    errorToast(error);
-    throw error;
-  }
-};
+  const transactions = await nwcClient.listTransactions({
+    limit: TRANSACTIONS_PAGE_SIZE,
+    offset: (page - 1) * TRANSACTIONS_PAGE_SIZE,
+    unpaid_outgoing: true,
+  });
+  return transactions;
+});
 
 export function useTransactions(page = 1) {
   const nwcClient = useAppStore((store) => store.nwcClient);
