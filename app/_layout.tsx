1715179e1cedde11ad609e9ac7f66ecdc53a6dc2
im-adithya <imadithyavardhan@gmail.com>
2025-07-28 10:30:54 +0530

    chore: remove unnecessary sentry config params

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,18 +29,6 @@ import { registerForPushNotificationsAsync } from "~/services/Notifications";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
-
-  // Adds more context data to events (IP address, cookies, user, etc.)
-  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
-  sendDefaultPii: true,
-
-  // Configure Session Replay
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration()],
-
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: __DEV__,
 });
 
 const LIGHT_THEME: Theme = {
