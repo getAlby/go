990ea428d64eeaf0f94fb89e4765f3894bf5b68d
im-adithya <imadithyavardhan@gmail.com>
2025-07-28 16:02:52 +0530

    fix: layout when there are no contacts

--- pages/send/Manual.tsx
+++ pages/send/Manual.tsx
@@ -48,115 +48,126 @@ export function Manual() {
     <>
       <Screen title="Manual Entry" />
       <DismissableKeyboardView>
-        <View className="flex-1 h-full flex flex-col gap-6 p-6">
-          <View className="flex items-center justify-center mt-8 gap-6">
-            <Text className="text-muted-foreground text-center">
-              Type or paste a Lightning Address, lightning invoice or LNURL.
-            </Text>
-            <Input
-              className="w-full text-center border-transparent bg-transparent native:text-2xl font-semibold2"
-              placeholder="hello@getalby.com"
-              value={keyboardText}
-              onChangeText={setKeyboardText}
-              autoCapitalize="none"
-              inputMode="email"
-              returnKeyType="done"
-              autoFocus={!addressBookEntries.length}
-            />
-          </View>
-          {addressBookEntries.length > 0 && (
-            <ScrollView className="flex-1 flex flex-col mt-2 opacity-75">
-              {addressBookEntries
-                .filter(
-                  (entry) =>
-                    !keyboardText ||
-                    entry.lightningAddress?.includes(keyboardText) ||
-                    entry.name
-                      ?.toLowerCase()
-                      .includes(keyboardText.toLowerCase()),
-                )
-                .map((addressBookEntry, index) => (
-                  <TouchableOpacity
-                    key={index}
-                    onPress={() => {
-                      router.dismissAll();
-                      router.navigate({
-                        pathname: "/send",
-                        params: {
-                          url: addressBookEntry.lightningAddress,
-                        },
-                      });
-                    }}
-                    className="mb-2"
-                  >
-                    <Card className="border-0 shadow-none">
-                      <CardContent className="flex flex-row items-center gap-4">
-                        <View className="h-10 w-10 flex items-center justify-center rounded-full bg-accent">
-                          <Text className="text-foreground text-base font-bold">
-                            {addressBookEntry.name?.[0]?.toUpperCase() ||
-                              addressBookEntry.lightningAddress?.[0]?.toUpperCase() ||
-                              "SN"}
-                          </Text>
-                        </View>
-                        <View className="flex flex-1 flex-col">
-                          <CardTitle>
-                            {addressBookEntry.name ||
-                              addressBookEntry.lightningAddress}
-                          </CardTitle>
-                          <CardDescription>
-                            {addressBookEntry.lightningAddress}
-                          </CardDescription>
-                        </View>
-                      </CardContent>
-                    </Card>
-                  </TouchableOpacity>
-                ))}
-            </ScrollView>
-          )}
-          {LN_ADDRESS_REGEX.test(keyboardText) &&
-            !addressBookEntries.some(
-              (entry) => entry.lightningAddress === keyboardText,
-            ) && (
-              <View className="flex items-center justify-center">
-                <Text className="text-muted-foreground text-center flex flex-row gap-4">
-                  <Checkbox
-                    aria-labelledby="add-to-contacts"
-                    checked={addToContacts}
-                    onCheckedChange={setAddToContacts}
-                  />{" "}
-                  <View>
-                    <Label nativeID="add-to-contacts">Add to contacts</Label>
-                  </View>
-                </Text>
-              </View>
-            )}
-          {addToContacts && keyboardText.includes("@") && (
-            <View>
-              <Label nativeID="name" className="self-start justify-self-start">
-                Name
-              </Label>
+        <View className="flex-1 flex flex-col">
+          <View className="flex-1 flex flex-col gap-6 p-6">
+            <View className="flex items-center justify-center mt-8 gap-6">
+              <Text className="text-muted-foreground text-center">
+                Type or paste a Lightning Address, lightning invoice or LNURL.
+              </Text>
               <Input
-                autoFocus
-                placeholder="Satoshi"
+                className="border-transparent bg-transparent native:text-2xl font-semibold2"
+                placeholder="hello@getalby.com"
+                value={keyboardText}
+                onChangeText={setKeyboardText}
+                autoCapitalize="none"
                 inputMode="email"
-                className="w-full"
-                value={contactName}
-                onChangeText={setContactName}
-                aria-labelledbyledBy="name"
                 returnKeyType="done"
-                // aria-errormessage="inputError"
+                autoFocus={!addressBookEntries.length}
               />
             </View>
-          )}
-          <Button
-            onPress={submitKeyboardText}
-            disabled={!keyboardText || isSubmitting}
-            size="lg"
-            className="flex flex-row gap-2"
-          >
-            {isSubmitting && <Loading className="text-primary-foreground" />}
-            <Text>Next</Text>
-          </Button>
+            {addressBookEntries.length > 0 && (
+              <ScrollView className="flex-1 flex flex-col mt-2 opacity-75">
+                {addressBookEntries
+                  .filter(
+                    (entry) =>
+                      !keyboardText ||
+                      entry.lightningAddress?.includes(keyboardText) ||
+                      entry.name
+                        ?.toLowerCase()
+                        .includes(keyboardText.toLowerCase()),
+                  )
+                  .map((addressBookEntry, index) => (
+                    <TouchableOpacity
+                      key={index}
+                      onPress={() => {
+                        router.dismissAll();
+                        router.navigate({
+                          pathname: "/send",
+                          params: {
+                            url: addressBookEntry.lightningAddress,
+                          },
+                        });
+                      }}
+                      className="mb-2"
+                    >
+                      <Card className="border-0 shadow-none">
+                        <CardContent className="flex flex-row items-center gap-4">
+                          <View className="h-10 w-10 flex items-center justify-center rounded-full bg-accent">
+                            <Text className="text-foreground text-base font-bold">
+                              {addressBookEntry.name?.[0]?.toUpperCase() ||
+                                addressBookEntry.lightningAddress?.[0]?.toUpperCase() ||
+                                "SN"}
+                            </Text>
+                          </View>
+                          <View className="flex flex-1 flex-col">
+                            <CardTitle>
+                              {addressBookEntry.name ||
+                                addressBookEntry.lightningAddress}
+                            </CardTitle>
+                            <CardDescription>
+                              {addressBookEntry.lightningAddress}
+                            </CardDescription>
+                          </View>
+                        </CardContent>
+                      </Card>
+                    </TouchableOpacity>
+                  ))}
+              </ScrollView>
+            )}
+          </View>
+          <View className="p-6 flex flex-col gap-4">
+            <View>
+              {LN_ADDRESS_REGEX.test(keyboardText) &&
+                !addressBookEntries.some(
+                  (entry) => entry.lightningAddress === keyboardText,
+                ) && (
+                  <View className="flex items-center justify-center">
+                    <Text className="text-muted-foreground text-center flex flex-row gap-4">
+                      <Checkbox
+                        aria-labelledby="add-to-contacts"
+                        checked={addToContacts}
+                        onCheckedChange={setAddToContacts}
+                      />{" "}
+                      <View>
+                        <Label nativeID="add-to-contacts">
+                          Add to contacts
+                        </Label>
+                      </View>
+                    </Text>
+                  </View>
+                )}
+              {addToContacts && keyboardText.includes("@") && (
+                <View>
+                  <Label
+                    nativeID="name"
+                    className="self-start justify-self-start"
+                  >
+                    Name
+                  </Label>
+                  <Input
+                    autoFocus
+                    placeholder="Satoshi"
+                    inputMode="email"
+                    className="w-full"
+                    value={contactName}
+                    onChangeText={setContactName}
+                    aria-labelledbyledBy="name"
+                    returnKeyType="done"
+                    // aria-errormessage="inputError"
+                  />
+                </View>
+              )}
+            </View>
+            <Button
+              onPress={submitKeyboardText}
+              disabled={!keyboardText || isSubmitting}
+              size="lg"
+              className="flex flex-row gap-2"
+            >
+              {isSubmitting && <Loading className="text-primary-foreground" />}
+              <Text>Next</Text>
+            </Button>
+          </View>
         </View>
       </DismissableKeyboardView>
     </>
