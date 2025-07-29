76b04b4a744ef360cabd1e9b08aa561434f2d2ed
im-adithya <imadithyavardhan@gmail.com>
2025-07-29 10:48:28 +0530

    chore: bump version to 1.14.0

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default ({ config }) => {
     ...config,
     name: "Alby Go",
     slug: "alby-mobile",
-    version: "1.13.1",
+    version: "1.14.0",
     scheme: [
       "lightning",
       "bitcoin",
