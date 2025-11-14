cfb809ee0b33ed680649bac7ef8db22749963ce8
im-adithya <imadithyavardhan@gmail.com>
2025-11-14 18:18:26 +0530

    feat: bring btc map home

--- components/Screen.tsx
+++ components/Screen.tsx
@@ -1,19 +1,21 @@
-import { type NativeStackHeaderRightProps } from "@react-navigation/native-stack";
+import { type NativeStackHeaderItemProps } from "@react-navigation/native-stack";
 import { Stack } from "expo-router";
 import { type StackAnimationTypes } from "react-native-screens";
 
 type ScreenProps = {
   title: string;
-  right?: (props: NativeStackHeaderRightProps) => React.ReactNode;
+  left?: (props: NativeStackHeaderItemProps) => React.ReactNode;
+  right?: (props: NativeStackHeaderItemProps) => React.ReactNode;
   animation?: StackAnimationTypes;
 };
 
-function Screen({ title, animation, right }: ScreenProps) {
+function Screen({ title, animation, right, left }: ScreenProps) {
   return (
     <Stack.Screen
       options={{
         title,
         animation,
+        headerLeft: left ? left : undefined,
         headerRight: right ? right : undefined,
         headerShadowVisible: false,
       }}
