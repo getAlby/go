bdcf688b4b238121e2d26b9f5ad469e314366fbe
im-adithya <imadithyavardhan@gmail.com>
2025-11-24 21:01:47 +0530

    fix: settings page

--- pages/settings/Settings.tsx
+++ pages/settings/Settings.tsx
@@ -19,7 +19,7 @@ import AlbyBanner from "~/components/AlbyBanner";
 import Screen from "~/components/Screen";
 import { Text } from "~/components/ui/text";
 import { useSession } from "~/hooks/useSession";
-import { DEFAULT_WALLET_NAME, IS_EXPO_GO } from "~/lib/constants";
+import { IS_EXPO_GO } from "~/lib/constants";
 import { deregisterWalletNotifications } from "~/lib/notifications";
 import { removeAllInfo } from "~/lib/notificationsNativeStorage";
 import { useAppStore } from "~/lib/state/appStore";
@@ -27,8 +27,6 @@ import { useColorScheme } from "~/lib/useColorScheme";
 
 export function Settings() {
   const wallets = useAppStore((store) => store.wallets);
-  const wallet = useAppStore((store) => store.wallets[store.selectedWalletId]);
-  const fiatCurrency = useAppStore((store) => store.fiatCurrency);
   const [developerCounter, setDeveloperCounter] = React.useState(0);
   const [developerMode, setDeveloperMode] = React.useState(__DEV__);
   const { colorScheme, toggleColorScheme } = useColorScheme();
@@ -40,38 +38,38 @@ export function Settings() {
       <View className="flex-1 flex flex-col gap-6">
         <Link href="/settings/wallets" asChild>
           <TouchableOpacity className="flex flex-row items-center gap-4">
-            <WalletIcon className="text-muted-foreground" />
+            <WalletIcon
+              className="text-muted-foreground"
+              width={28}
+              height={28}
+            />
             <Text className="font-medium2 text-xl text-foreground">
               Wallets
-            </Text>
-            <Text
-              className="text-muted-foreground text-xl flex-shrink"
-              numberOfLines={1}
-              ellipsizeMode="tail"
-            >
-              ({wallet.name || DEFAULT_WALLET_NAME})
             </Text>
           </TouchableOpacity>
         </Link>
 
         <Link href="/settings/fiat-currency" asChild>
           <TouchableOpacity className="flex flex-row gap-4 items-center">
-            <BitcoinIcon className="text-muted-foreground" />
+            <BitcoinIcon
+              className="text-muted-foreground"
+              width={28}
+              height={28}
+            />
             <Text className="text-foreground font-medium2 text-xl">
               Fiat Currency
             </Text>
-            {fiatCurrency && (
-              <Text className="text-muted-foreground text-xl">
-                ({fiatCurrency})
-              </Text>
-            )}
           </TouchableOpacity>
         </Link>
 
         {!IS_EXPO_GO && (
           <Link href="/settings/notifications" asChild>
             <TouchableOpacity className="flex flex-row gap-4 items-center">
-              <NotificationIcon className="text-muted-foreground" />
+              <NotificationIcon
+                className="text-muted-foreground"
+                width={28}
+                height={28}
+              />
               <Text className="text-foreground font-medium2 text-xl">
                 Notifications
               </Text>
@@ -81,7 +79,11 @@ export function Settings() {
 
         <Link href="/settings/security" asChild>
           <TouchableOpacity className="flex flex-row gap-4 items-center">
-            <FingerprintIcon className="text-muted-foreground" />
+            <FingerprintIcon
+              className="text-muted-foreground"
+              width={28}
+              height={28}
+            />
             <Text className="text-foreground font-medium2 text-xl">
               Security
             </Text>
@@ -92,7 +94,7 @@ export function Settings() {
           className="flex flex-row gap-4 items-center"
           onPress={toggleColorScheme}
         >
-          <ThemeIcon className="text-muted-foreground" />
+          <ThemeIcon className="text-muted-foreground" width={28} height={28} />
           <Text className="text-foreground font-medium2 text-xl">Theme</Text>
           <Text className="text-muted-foreground text-xl">
             ({colorScheme.charAt(0).toUpperCase() + colorScheme.substring(1)})
@@ -101,7 +103,11 @@ export function Settings() {
 
         <Link href="/settings/address-book" asChild>
           <TouchableOpacity className="flex flex-row gap-4 items-center">
-            <AddressIcon className="text-muted-foreground" />
+            <AddressIcon
+              className="text-muted-foreground"
+              width={28}
+              height={28}
+            />
             <Text className="text-foreground font-medium2 text-xl">
               Address Book
             </Text>
@@ -120,7 +126,11 @@ export function Settings() {
                   signOut();
                 }}
               >
-                <SignOutIcon className="text-muted-foreground" />
+                <SignOutIcon
+                  className="text-muted-foreground"
+                  width={28}
+                  height={28}
+                />
                 <Text className="font-medium2 text-xl">Sign out</Text>
               </TouchableOpacity>
               <TouchableOpacity
@@ -130,7 +140,11 @@ export function Settings() {
                   useAppStore.getState().setOnboarded(false);
                 }}
               >
-                <OnboardingIcon className="text-muted-foreground" />
+                <OnboardingIcon
+                  className="text-muted-foreground"
+                  width={28}
+                  height={28}
+                />
                 <Text className="font-medium2 text-xl">Open Onboarding</Text>
               </TouchableOpacity>
               <TouchableOpacity
@@ -161,7 +175,11 @@ export function Settings() {
                   );
                 }}
               >
-                <ResetIcon className="text-destructive" />
+                <ResetIcon
+                  className="text-destructive"
+                  width={28}
+                  height={28}
+                />
                 <Text className="text-destructive font-medium2 text-xl">
                   Reset Wallet
                 </Text>
