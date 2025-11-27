9df862320233a39df33afef7b5b54556c03e3160
im-adithya <imadithyavardhan@gmail.com>
2025-11-27 11:35:55 +0530

    chore: use primary instead of amber-300

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -20,7 +20,7 @@ function SwitchWeb({
     <SwitchPrimitives.Root
       className={cn(
         "peer flex-row h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed",
-        props.checked ? "bg-amber-300" : "bg-input",
+        props.checked ? "bg-primary" : "bg-input",
         props.disabled && "opacity-50",
         className,
       )}
