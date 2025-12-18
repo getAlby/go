13d8f2b6d023b34c13e3d570788c2aea7fb22e2f
im-adithya <imadithyavardhan@gmail.com>
2025-12-18 15:14:09 +0530

    fix: center screen title in android

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -100,7 +100,7 @@ export function Home() {
             onPressIn={() => {
               router.navigate("/settings");
             }}
-            className="-ml-4 px-6"
+            className="-ml-4 py-2 px-6"
           >
             <SettingsLineIcon
               className="text-secondary-foreground"
