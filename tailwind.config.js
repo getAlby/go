4186830b0695387833ae90e756c483d0ca2a18c5
im-adithya <imadithyavardhan@gmail.com>
2025-12-19 17:30:09 +0530

    fix: text sizing in different screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
+      fontSize: {
+        xs: ["12px", { lineHeight: "16px" }],
+        sm: ["14px", { lineHeight: "20px" }],
+        base: ["16px", { lineHeight: "24px" }],
+        lg: ["18px", { lineHeight: "28px" }],
+        xl: ["20px", { lineHeight: "28px" }],
+        "2xl": ["24px", { lineHeight: "32px" }],
+        "3xl": ["30px", { lineHeight: "36px" }],
+        "4xl": ["36px", { lineHeight: "40px" }],
+        "5xl": ["48px", { lineHeight: "1" }],
+        "6xl": ["60px", { lineHeight: "1" }],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
